0152a9505ee00b4523926736fa1689e89698d07f
NightmanCZ90 <novak.jan90@gmail.com>
2020-03-03 23:38:32 +0100

    exercise 3-3

--- exercises_2.py
+++ exercises_2.py
@@ -102,3 +102,87 @@ def two_oldest_ages(lst):
 
 
 # two_oldest_ages( [4,25,3,20,19,5] )
+
+'''
+is_odd_string('a') # True
+is_odd_string('aaaa') # False
+is_odd_string('amazing') # True
+is_odd_string('veryfun') # True
+is_odd_string('veryfunny') # False
+'''
+
+def is_odd_string(str):
+    abcd = "0abcdefghijklmnopqrstuvwxyz"
+    total = 0
+    for letter in str:
+        # print(letter)
+        total += abcd.index(letter)
+    if total % 2 == 0:
+        return False
+    return True
+
+# is_odd_string('amazing')
+
+'''
+valid_parentheses("()") # True 
+valid_parentheses(")(()))") # False 
+valid_parentheses("(") # False 
+valid_parentheses("(())((()())())") # True 
+valid_parentheses('))((') # False
+valid_parentheses('())(') # False
+valid_parentheses('()()()()())()(') # False
+'''
+
+def valid_parentheses(str):
+    if str[0] == "(" and str[-1] == ")":
+        return True
+    return False
+
+
+'''
+reverse_vowels("Hello!") # "Holle!" 
+reverse_vowels("Tomatoes") # "Temotaos" 
+reverse_vowels("Reverse Vowels In A String") # "RivArsI Vewols en e Streng"
+reverse_vowels("aeiou") # "uoiea"
+reverse_vowels("why try, shy fly?") # "why try, shy fly?"
+'''
+
+def reverse_vowels(str):
+    vowels = "aeiouAEIOU"
+    new_str = ""
+    to_reverse = ""
+    for letter in str:
+        if letter in vowels:
+            new_str += "*"
+            to_reverse+= letter
+            # print(str)
+            # print(new_str)
+            # print(to_reverse)
+        else: new_str += letter
+    print(new_str)
+    reverse = to_reverse[::-1]
+    # print(reverse)
+    final_str = ""
+    count = 0
+    for letter in new_str:
+        if letter == "*":
+            final_str += reverse[count]
+            count += 1
+        else: final_str += letter
+    print(final_str)
+    return final_str
+
+# reverse_vowels("Reverse Vowels In A String")
+
+'''
+three_odd_numbers([1,2,3,4,5]) # True
+three_odd_numbers([0,-2,4,1,9,12,4,1,0]) # True
+three_odd_numbers([5,2,1]) # False
+three_odd_numbers([1,2,3,3,2]) # False
+'''
+
+def three_odd_numbers(lst):
+    for i in range(len(lst)-2):
+        if (lst[i]+lst[i+1]+lst[i+2]) % 2 != 0:
+            return True
+    return False
